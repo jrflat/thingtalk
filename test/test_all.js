40d9016073c1dab46e91dadfe56f3214d6e1bc37
Silei Xu <silei@cs.stanford.edu>
2019-02-04 22:27:44 -0800

    wip: sql compiler
    - add tests using the new ThingTalk syntax
    - add translation from query to sql, still missing translation for monitors
    - suppport invocation/project/join/filter, still missing aggregations
    - scope is not tested because the statement won't typecheck and it will be messy to add scope to typecheck api just for testing

--- test/test_all.js
+++ test/test_all.js
@@ -42,5 +42,6 @@ seq([
     ('./test_formatter_api'),
     ('./test_class_to_manifest'),
     ('./test_schema_retriever'),
-    //('./test_sql_compiler'),
+    ('./test_sql_compiler'),
+    ('./test_sentence_generator')
 ]);
